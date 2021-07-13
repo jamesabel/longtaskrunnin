b529d1f7ed67feb1919e8eab62435665dcbaeead
James Abel <j@abel.co>
2021-07-13 01:26:31 -0700

    make app logging similar to testing

--- longtaskrunnin/__main__.py
+++ longtaskrunnin/__main__.py
@@ -1,15 +1,16 @@
 import sys
+
 from ismain import is_main
 from balsa import Balsa, get_logger
 from PyQt5.Qt import QApplication
 
-from longtaskrunnin import LongTaskRunnin, application_name, author, options_str
+from longtaskrunnin import LongTaskRunnin, application_name, author
 
 log = get_logger(application_name)
 
 if is_main():
 
-    balsa = Balsa(application_name, author, verbose=False)
+    balsa = Balsa(application_name, author, log_directory="log", delete_existing_log_files=True, verbose=True)
     balsa.init_logger()
 
     application = QApplication(sys.argv)
