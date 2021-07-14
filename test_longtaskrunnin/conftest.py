7682dd487254faa3ed65fe1615e927e8792a3515
James Abel <j@abel.co>
2021-07-13 20:06:04 -0700

    don't need to create a fixture for QtBot - pytest-qt apparently has one built-in

--- test_longtaskrunnin/conftest.py
+++ test_longtaskrunnin/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from pytestqt.qtbot import QtBot
 from balsa import Balsa
 
 from longtaskrunnin import application_name, author
@@ -9,8 +8,3 @@ from longtaskrunnin import application_name, author
 def long_task_runnin_session():
     balsa = Balsa(application_name, author, delete_existing_log_files=True, log_directory="log", verbose=False)
     balsa.init_logger()
-
-
-@pytest.fixture
-def qtbot(qapp, request):
-    return QtBot(request)
