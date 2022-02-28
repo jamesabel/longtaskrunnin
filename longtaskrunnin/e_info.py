517ec6f1a5d63b40385157b336a3965e833e77ed
James Abel <j@abel.co>
2022-02-27 21:51:19 -0800

    blackify

--- longtaskrunnin/e_info.py
+++ longtaskrunnin/e_info.py
@@ -7,6 +7,7 @@ class EInfo:
     """
     Information on the calculation of "e" (the natural log).
     """
+
     uuid: str = str(uuid.uuid4())  # this will be the UUID of the "global" instance
     e_value: float = 0.0
     duration: float = 0.0
--- test_longtaskrunnin/__init__.py
+++ test_longtaskrunnin/__init__.py
@@ -1,2 +1,1 @@
-
 from .conftest import balsa_factory
